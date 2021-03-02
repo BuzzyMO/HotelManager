e87b84f61a6c46cbb546d113eded127d12f2e144
BuzzyMO <oleksandr.lynnyk@nure.ua>
2021-03-02 22:47:56 +0200

    fix load property using Tomcat

--- src/main/java/com/example/hotelmanager/connection/ConnectionProviderImpl.java
+++ src/main/java/com/example/hotelmanager/connection/ConnectionProviderImpl.java
@@ -1,5 +1,7 @@
 package com.example.hotelmanager.connection;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
@@ -18,8 +20,8 @@ public class ConnectionProviderImpl implements ConnectionProvider{
     @Override
     public Connection getConnection() throws SQLException{
         Properties properties = new Properties();
-        try(InputStream in = Files.newInputStream(Paths.get("src","main", "resources", "database.properties"))){
-            properties.load(in);
+        try{
+            properties.load(getClass().getClassLoader().getResourceAsStream("database.properties"));
         } catch (IOException ex){
             logger.fatal("Unable to open or read file", ex);
         }
@@ -28,8 +30,11 @@ public class ConnectionProviderImpl implements ConnectionProvider{
         String username = properties.getProperty("db.username");
         String password = properties.getProperty("db.password");
 
-        if(drivers != null) System.setProperty("jdbc.drivers", drivers);
-
+        try {
+            Class.forName(drivers);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
         return DriverManager.getConnection(url, username, password);
     }
 
